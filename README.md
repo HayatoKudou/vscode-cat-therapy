19bf75e1f8bc3dee4da55d44b2475958ebc80bae
kudou <kudoh115@gmail.com>
2021-07-05 22:03:41 +0900

    master

--- README.md
+++ README.md
@@ -1,34 +1,13 @@
-# Cat Coding — A Webview API Sample
+# Cat Therapy
 
-Demonstrates VS Code's [webview API](https://code.visualstudio.com/api/extension-guides/webview). This includes:
-
-- Creating and showing a basic webview.
-- Dynamically updating a webview's content.
-- Loading local content in a webview.
-- Running scripts in a webview.
-- Sending message from an extension to a webview.
-- Sending messages from a webview to an extension.
-- Using a basic content security policy.
-- Webview lifecycle and handling dispose.
-- Saving and restoring state when the panel goes into the background.
-- Serialization and persistence across VS Code reboots.
+Just press a button to see a gif image of a cat.
+Give your brain a break.
+Cats solve everything.
 
 ## Demo
 
 ![demo](demo.gif)
 
-## VS Code API
+## Available Commands
 
-### `vscode` module
-
-- [`window.createWebviewPanel`](https://code.visualstudio.com/api/references/vscode-api#window.createWebviewPanel)
-- [`window.registerWebviewPanelSerializer`](https://code.visualstudio.com/api/references/vscode-api#window.registerWebviewPanelSerializer)
-
-## Running the example
-
-- Open this example in VS Code 1.47+
-- `npm install`
-- `npm run watch` or `npm run compile`
-- `F5` to start debugging
-
-Run the `Cat Coding: Start cat coding session` to create the webview.
+* `Cat Command: get 100 new cats` Swap GIF of 100 cats
--- src/extension.ts
+++ src/extension.ts
@@ -1,77 +1,62 @@
 import * as vscode from 'vscode';
+import axios from "axios";
 
-let myStatusBarItem: vscode.StatusBarItem;
-
-const cats = {
-	'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
-	'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
-	'Testing Cat': 'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif'
-};
+var cats: string[] = [];
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const quickPick = 'terminalLog.quickPick';
+	getCatsGif();
+	
+	const callCat = 'catCommand.callCat';
 
-	context.subscriptions.push(vscode.commands.registerCommand(quickPick, () => {
-		if (vscode.window.registerWebviewPanelSerializer) {
-			vscode.window.registerWebviewPanelSerializer(CatCodingPanel.viewType, {
-				async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
-					webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
-					CatCodingPanel.revive(webviewPanel, context.extensionUri);
-				}
-			});
-		} else {
-			vscode.window.showWarningMessage('失敗');
-		}
+	context.subscriptions.push(vscode.commands.registerCommand(callCat, () => {
+		const panel = vscode.window.createWebviewPanel(
+			'catCommand',
+			'This is a cat',
+			vscode.ViewColumn.One,
+			{}
+		);
+		panel.webview.html = getWebviewContent();
 	}));
 
-	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	myStatusBarItem.command = quickPick;
-	myStatusBarItem.text = 'Cat!!';
+	context.subscriptions.push(vscode.commands.registerCommand('catCommand.changeCats', () => {
+		getCatsGif();
+	}))
+
+	const myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	myStatusBarItem.command = callCat;
+	myStatusBarItem.text = `$(comment) Hey Cat!!`;
 	context.subscriptions.push(myStatusBarItem);
 	myStatusBarItem.show();
 }
 
-// webview設定
-function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
-	return {
-		enableScripts: true,
-		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
-	};
+function getCatsGif() {
+	cats = [];
+	const search = "cat";
+	const key = "VHVLGLNA2hlgRyYY1MaxMFXTuS3Lus38";
+	const limit = 100;
+
+	const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+
+	axios.get(url).then(res => {
+		const data = res.data.data;
+		data.map((val: any) => {
+			cats.push(val.images.downsized.url);
+		})
+	});
 }
 
-class CatCodingPanel {
-
-	public static readonly viewType = 'catCoding';
-	public static currentPanel: CatCodingPanel | undefined;
-	private readonly _panel: vscode.WebviewPanel;
-	private readonly _extensionUri: vscode.Uri;
-
-	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-		this._panel = panel;
-		this._extensionUri = extensionUri;
-		const webview = this._panel.webview;
-
-		this._updateForCat(webview, 'Compiling Cat');
-	}
-
-	private _updateForCat(webview: vscode.Webview, catName: keyof typeof cats) {
-		this._panel.title = catName;
-		this._panel.webview.html = this._getHtmlForWebview(webview, cats[catName]);
-	}
-
-	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-		CatCodingPanel.currentPanel = new CatCodingPanel(panel, extensionUri);
-	}
-
-	private _getHtmlForWebview(webview: vscode.Webview, catGifPath: string) {
-
-		return `<!DOCTYPE html>
-			<html lang="en">
-			<body>
-				<img src="${catGifPath}" width="300" />
-				<h1 id="lines-of-code-counter">0</h1>
-			</body>
-			</html>`;
-	}
+function getWebviewContent() {
+	const cat = cats[Math.floor(Math.random() * cats.length)];
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="${cat}" width="50%" />
+</body>
+</html>`;
 }
